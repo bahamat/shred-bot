d4e60cb92ac3dc90e5859594d957fc446acbe16a
Brianna Bennett <brie@zonename.org>
2025-10-20 10:22:25 -0700

    Move REST endpoints to dedicated file

--- scripts/routes.mjs
+++ scripts/routes.mjs
@@ -1,11 +1,14 @@
+// Description:
+//     Give the bot a personality.
+
 export default async robot => {
     robot.router.post('/hubot/chatsecrets/:room', async (req, res) => {
         const room = req.params.room
         const data = JSON.parse(req.body.payload)
         const secret = data.secret
-    
+
         await robot.messageRoom(room, `I have a secret: ${secret}`)
-    
+
         await res.send('OK')
     });
 }
