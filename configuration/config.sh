4ac0d5fde766701f546a28e5d295de657cfcb509
Brianna Bennett <brie.bennett@fastly.com>
2025-10-19 18:16:38 -0700

    Fix local config parsing

--- configuration/config.sh
+++ configuration/config.sh
@@ -2,8 +2,8 @@ TOP=$(git rev-parse --show-toplevel)
 
 export HUBOT_ANSWER_TO_THE_ULTIMATE_QUESTION_OF_LIFE_THE_UNIVERSE_AND_EVERYTHING=42
 
-if [[ -f ${TOP}/config.local.sh ]]; then
-    source ${TOP}/config.local.sh
+if [[ -f ${TOP}/configuration/config.local.sh ]]; then
+    source ${TOP}/configuration/config.local.sh
 fi
 
 export HUBOT_MAX_MESSAGE_LENGTH="2000"
