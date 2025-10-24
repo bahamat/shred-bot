3a290c9533224d0ed446cbf62a725d4db8c6b32d
Brianna Bennett <brie@zonename.org>
2025-10-24 13:44:06 -0700

    Fix routes

--- scripts/routes.mjs
+++ scripts/routes.mjs
@@ -4,7 +4,7 @@
 export default async robot => {
     robot.router.post('/hubot/chatsecrets/:room', async (req, res) => {
         const room = req.params.room
-        const data = JSON.parse(req.body.payload)
+        const data = req.body
         const secret = data.secret
 
         await robot.messageRoom(room, `I have a secret: ${secret}`)
@@ -15,6 +15,6 @@ export default async robot => {
     robot.router.get('/hubot/ping', async (req, res) => {
         var pong = { ping: "pong"}
 
-        await res.send(JSON.stringify(pong))
+        await res.send(JSON.stringify(pong) + '\n')
     });
 }
